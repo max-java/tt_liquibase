c184c3fa6333c6a9d9cf2bb6d5d99c64a88a321e
max-java <6666350@gmail.com>
2020-12-13 01:53:09 +0300

    adds sql example

--- src/main/resources/db/changelog/createDatabase.sql
+++ src/main/resources/db/changelog/createDatabase.sql
@@ -1,4 +1,5 @@
 # https://docs.liquibase.com/change-types/community/add-column.html
+# https://docs.liquibase.com/change-types/community/sql.html
 # just a cheat sheet to create database and user;
 CREATE DATABASE TT_LIQUIBASE
     DEFAULT CHARACTER SET utf8
